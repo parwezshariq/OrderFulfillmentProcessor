e7f2685a9b5743e6632cb6d4cbe9f6489a661c00
shariqparwez <shariqparwez@outlook.com>
2019-01-26 16:15:36 +0530

    SQL Component - Routing Rule Added

--- src/main/java/com/shariqparwez/orderfulfillment/config/IntegrationConfig.java
+++ src/main/java/com/shariqparwez/orderfulfillment/config/IntegrationConfig.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.sql.DataSource;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.sql.SqlComponent;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+
+import com.shariqparwez.orderfulfillment.order.OrderStatus;
 
 @Configuration
 public class IntegrationConfig extends CamelConfiguration {
@@ -16,7 +21,36 @@ public class IntegrationConfig extends CamelConfiguration {
 	@Inject
 	private Environment environment;
 	
-	@Override
+	@Inject
+	private DataSource dataSource;
+	
+	@Bean
+	public SqlComponent sql() {
+		SqlComponent sqlComponent = new SqlComponent();
+		sqlComponent.setDataSource(dataSource);
+		return sqlComponent;
+	}
+	
+	@Bean
+	public RouteBuilder newWebsiteOrderRoute() {
+		return new RouteBuilder() {
+			
+			@Override
+			public void configure() throws Exception {
+				from("sql:"
+						+ "select id from orders.\"ordertype\" where status = '"
+						+ OrderStatus.NEW.getCode()
+						+ "'"
+						+ "?"
+						+ "consumer.onConsume=update orders.\"ordertype\" set status = '"
+						+ OrderStatus.PROCESSING.getCode()
+						+ "' where id = :#id")
+				.to("log:com.shariqparwez.orderfulfillment.order?level=INFO");
+			}
+		};
+	}
+	
+	/*@Override
 	public List<RouteBuilder> routes(){
 		List<RouteBuilder> routeList = new ArrayList<RouteBuilder>();
 		
@@ -32,6 +66,6 @@ public class IntegrationConfig extends CamelConfiguration {
 		});
 		
 		return routeList;
-	}
+	}*/
 	
 }
