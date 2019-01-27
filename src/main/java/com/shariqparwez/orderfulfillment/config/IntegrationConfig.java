103effc9dbb3c1e2a32baf764ea6368dc1fd0595
shariqparwez <shariqparwez@outlook.com>
2019-01-27 18:57:54 +0530

    ActiveMQ / JMS Integration

--- src/main/java/com/shariqparwez/orderfulfillment/config/IntegrationConfig.java
+++ src/main/java/com/shariqparwez/orderfulfillment/config/IntegrationConfig.java
@@ -4,9 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
 import javax.sql.DataSource;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.activemq.pool.PooledConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsConfiguration;
 import org.apache.camel.component.sql.SqlComponent;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -23,6 +28,33 @@ public class IntegrationConfig extends CamelConfiguration {
 	
 	@Inject
 	private DataSource dataSource;
+	
+	@Bean
+	public ConnectionFactory jmsConnectionFactory() {
+		return new ActiveMQConnectionFactory(environment.getProperty("activemq.broker.url"));
+	}
+	
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public PooledConnectionFactory pooledConnectionFactory() {
+		PooledConnectionFactory factory = new PooledConnectionFactory();
+		factory.setConnectionFactory(jmsConnectionFactory());
+		factory.setMaxConnections(Integer.parseInt(environment.getProperty("pooledConnectionFactory.maxConnections")));
+		return factory;
+	}
+	
+	@Bean
+	public JmsConfiguration jmsConfiguration() {
+		JmsConfiguration jmsConfiguration = new JmsConfiguration();
+		jmsConfiguration.setConnectionFactory(pooledConnectionFactory());
+		return jmsConfiguration;
+	}
+	
+	@Bean
+	public ActiveMQComponent activeMq() {
+		ActiveMQComponent activeMq = new ActiveMQComponent();
+		activeMq.setConfiguration(jmsConfiguration());
+		return activeMq;
+	}
 	
 	@Bean
 	public SqlComponent sql() {
@@ -46,7 +78,8 @@ public class IntegrationConfig extends CamelConfiguration {
 						+ OrderStatus.PROCESSING.getCode()
 						+ "' where id = :#id")
 				.beanRef("orderItemMessageTranslator", "transformToOrderItemMessage")
-				.to("log:com.shariqparwez.orderfulfillment.order?level=INFO");
+				//.to("log:com.shariqparwez.orderfulfillment.order?level=INFO");
+				.to("activemq:queue:ORDER_ITEM_PROCESSING");
 			}
 		};
 	}
